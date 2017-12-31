119517b23558f3e06b566660d73ff3369192f466
wkatbey <lunarxparallax@gmail.com>
2017-12-30 23:51:30 -0500

    This is another commit. I'm just reorganizing some files

---
+++ ProjectType.h
@@ -0,0 +1,80 @@
+//
+//  ProjectType.hpp
+//  ProjectManagementApplication
+//
+//  Created by Wassim Katbey on 12/29/17.
+//  Copyright © 2017 Wayne State University. All rights reserved.
+//
+
+#ifndef ProjectType_H
+#define ProjectType_H
+#include "toolType.h"
+#include <map>
+
+
+class projectType : public toolType {
+  
+    friend ostream& operator<<(ostream& osObject, projectType project);
+    friend istream& operator>>(ostream& isObject, projectType project);
+
+public:
+    
+    enum projectInfo {INCOMPLETE, COMPLETE, ABANDONED};
+    
+    projectType(string name = "", string desc = "", projectInfo i = INCOMPLETE,
+                int start[6] = 0, int end[6] = 0);
+    
+    void setProjectName(string name);
+    void writeDescription(string desc);
+    
+    void setStartDate(int start[6]);
+    void setEndDate(int end[6]);
+
+    void completeProject();
+    void abandonProject();
+    
+    void employ(toolType employee);
+    void dismiss(toolType employee);
+    
+    
+    statusType getEmployeeStatus(toolType employee) const;
+    projectInfo getProgress() const;
+    
+    string getStartDate() const;
+    string getEndDate() const;
+    
+    string getDescription() const;
+    
+    void printEmployeeDatabase();
+    void printProjectInfo();
+    
+    
+    
+private:
+    
+    string projectName;
+    string description;
+    projectInfo progress;
+    
+    char startDate[8];
+    char endDate[8];
+    
+    map<string, toolType> employeeMap;
+    //map<string, toolType> toolMap;
+    
+};
+
+
+
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+#endif /* ProjectType_hpp */
